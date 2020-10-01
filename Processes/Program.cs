314d31a3dcd9e14fa7c4766bb4c674882e6c1060
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 22:25:49 +0300

    Add project files.

---
+++ Processes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Processes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] CurrentProcesses = Process.GetProcesses();
+            for (int i=0;i<CurrentProcesses.Length;i++)
+            {
+                Console.WriteLine(CurrentProcesses[i].ProcessName);
+            }
+        }
+    }
+}
